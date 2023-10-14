bc910cdef0c4f4b5e3855068bdd55bee078c9bd3
prachihirekha <prachihirekhan32@gmail.com>
2023-10-14 14:07:43 +0530

    java project by CRUD operation

--- student/src/main/java/com/student/student/controller/Controller.java
+++ student/src/main/java/com/student/student/controller/Controller.java
@@ -2,11 +2,11 @@ package com.student.student.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
-public class Controller {
-    @GetMapping("/request")
-    public String getBook(){
-        return "sout";
+public class controller {
+        @GetMapping(path = "/request")
+        public String getBook(){
+            return "sout";
+        }
     }
-}
+
